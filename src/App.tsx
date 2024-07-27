45141be9dbe5fd6bc178470a5c1297c22eb7c5ea
Borapox <muller.mdi@gmail.com>
2024-07-26 23:26:03 -0300

    modificaçao no app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,11 @@ import '@fontsource-variable/dosis';
 const App = () => {
   return (
     <div className="min-h-screen bg-gradient-to-b from-[#1e293b] to-[#2d4258] flex items-center justify-center">
-      <h1 className="text-white text-4xl">Hello, Tailwind!</h1>
+      
+      <h1 className="text-white text-4xl">
+        Hello, Tailwind!
+      </h1>
+
     </div>
   );
 };
